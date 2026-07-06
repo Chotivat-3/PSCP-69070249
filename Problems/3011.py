8bcebd1b246349e9b1efd3142c5c11a88fd7f099
Chotivat-3 <69070249@kmitl.ac.th>
2026-07-06 18:22:46 +0700

    Update 3011.py

---
+++ Problems/3011.py
@@ -0,0 +1,35 @@
+"""Colors"""
+c = []
+c.append(input())
+c.append(input())
+color = ""
+c1 = c[0]
+c2 = c[1]
+def c_ (x1,x2):
+    """Colors"""
+    for i in c :
+        if i != "Red" or i != "Yellow" or i != "Blue":
+            color = "Error" 
+            break
+    if x1 == x2 :
+        color  = f"{x1}"
+    else:
+        match x1:
+            case "Red":
+                if x2 == "Yellow":
+                    color  = "Orange"
+                else:
+                    color  = "Violet"
+            case "Yellow":
+                if x2 == "Red":
+                    color  = "Orange"
+                else:
+                    color  = "Green"
+            case "Blue":
+                if x2 == "Red":
+                    color  = "Violet"
+                else:
+                    color  = "Green"
+    return color
+color = c_(c1,c2)
+print(color)
